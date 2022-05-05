153ec032c71cf9ed1643a02935ca3df436a0bd30
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-05-05 13:09:37 +0300

    Minor refactoring and Linux compatibility.
    GitOrigin-Include: binary.py
    GitOrigin-Include: mz*
    GitOrigin-RevId: bdbbe30c004eec6dbc18955f7b5f7953998eb07b

--- binary.py
+++ binary.py
@@ -1,26 +1,33 @@
 def from_bytes(bytes):
     return int.from_bytes(bytes, byteorder='little')
 
+
 def read_block(bytes, offset, size):
     assert len(bytes) >= offset + size
     return bytes[offset:offset+size]
+
 
 def read_block_until_end(bytes, offset):
     assert len(bytes) >= offset
     return bytes[offset:]
 
+
 def read_byte(bytes, offset):
     return from_bytes(read_block(bytes, offset, 1))
 
+
 def read_word(bytes, offset):
     return from_bytes(read_block(bytes, offset, 2))
+
 
 def write_word(bytes, offset, data):
     bytes[offset] = data & 0xff
     bytes[offset+1] = (data >> 8) & 0xff
 
+
 def read_dword(bytes, offset):
     return from_bytes(read_block(bytes, offset, 4))
+
 
 def write_dword(bytes, offset, data):
     bytes[offset] = data & 0xff
@@ -28,11 +35,13 @@ def write_dword(bytes, offset, data):
     bytes[offset+2] = (data >> 16) & 0xff
     bytes[offset+3] = (data >> 24) & 0xff
 
+
 def read_virtual(info, bytes, address, size):
     for section in info['sections'].values():
         if section['address'] <= address <= section['address-end']:
             return read_block(bytes, section['raw-offset'] + address - section['address'], size)
     return None
+
 
 def read_virtual_until_zero(info, bytes, address):
     result = b''
@@ -45,12 +54,15 @@ def read_virtual_until_zero(info, bytes, address):
             return result
     return None
 
+
 def read_directory(info, bytes, directory):
     return read_virtual(info, bytes, info['directories'][directory]['address'], info['directories'][directory]['size'])
+
 
 def read_section(info, bytes, section):
     size = max(info['sections'][section]['raw-size'], info['sections'][section]['address-end'] - info['sections'][section]['address'])
     return read_block(bytes, info['sections'][section]['raw-offset'], info['sections'][section]['raw-size']) + b'\0'*(size - info['sections'][section]['raw-size'])
+
 
 class Pipe(object):
     def __init__(self, bytes):
