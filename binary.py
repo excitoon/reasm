30e4f2b371108e97a868756bdf50e549bee18b89
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-05-19 21:30:26 +0300

    Added `cv2dwarf` and made minor improvements.
    GitOrigin-Include: binary.py
    GitOrigin-Include: mz*
    GitOrigin-RevId: c520e1a234a4d1ae7e2414c1967d843f161a34b5

--- binary.py
+++ binary.py
@@ -1,40 +1,40 @@
 def from_bytes(bytes):
     return int.from_bytes(bytes, byteorder='little')
 
-
 def read_block(bytes, offset, size):
     assert len(bytes) >= offset + size
     return bytes[offset:offset+size]
-
 
 def read_block_until_end(bytes, offset):
     assert len(bytes) >= offset
     return bytes[offset:]
 
+def read_until_zero(bytes, address):
+    result = b''
+    i = address
+    while bytes[i] != 0:
+        result += read_block(bytes, i, 1)
+        i += 1
+    return result
 
 def read_byte(bytes, offset):
     return from_bytes(read_block(bytes, offset, 1))
 
-
 def read_word(bytes, offset):
     return from_bytes(read_block(bytes, offset, 2))
-
 
 def write_word(bytes, offset, data):
     bytes[offset] = data & 0xff
     bytes[offset+1] = (data >> 8) & 0xff
 
-
 def read_dword(bytes, offset):
     return from_bytes(read_block(bytes, offset, 4))
-
 
 def write_dword(bytes, offset, data):
     bytes[offset] = data & 0xff
     bytes[offset+1] = (data >> 8) & 0xff
     bytes[offset+2] = (data >> 16) & 0xff
     bytes[offset+3] = (data >> 24) & 0xff
-
 
 def read_virtual(info, bytes, address, size):
     for section in info['sections'].values():
@@ -44,7 +44,6 @@ def read_virtual(info, bytes, address, size):
             else:
                 return read_block(bytes, section['raw-offset'] + address - section['address'], size)
     return None
-
 
 def read_virtual_until_zero(info, bytes, address):
     result = b''
@@ -57,10 +56,8 @@ def read_virtual_until_zero(info, bytes, address):
             return result
     return None
 
-
 def read_directory(info, bytes, directory):
     return read_virtual(info, bytes, info['directories'][directory]['address'], info['directories'][directory]['size'])
-
 
 def read_section(info, bytes, section):
     size = max(info['sections'][section]['raw-size'], info['sections'][section]['address-end'] - info['sections'][section]['address'])
